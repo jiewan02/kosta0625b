88a2ce998748fd62882d40f4f401ce6f4c86a07d
jiewan02 <jiewan2002@gmail.com>
2025-06-25 12:15:57 +0900

    first commit

---
+++ src/main/java/com/example/demo/Kosta0625bApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kosta0625bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kosta0625bApplication.class, args);
+	}
+
+}
