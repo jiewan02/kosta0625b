775c06a45a8b5cac9f741677db537fe01d3a282c
jiewan02 <jiewan2002@gmail.com>
2025-06-25 15:11:26 +0900

    create DeptController

---
+++ src/main/java/com/example/demo/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeptController {
+
+	@GetMapping("/board/list")
+	public String list() {
+		return "게시물 목록"; 
+	}
+	
+}
